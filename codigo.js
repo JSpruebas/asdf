d0515aa0a0026be4a1110c3a01db64fca4545ea4
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-07 02:09:18 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -46,6 +46,8 @@ const coso = async () => {
   const resultado = await web3.eth.getBalance(cuenta)
   alert("El balance de " + cuenta + " es: " + resultado)
 
+  const zeroStratAbi = "./zeroStratAbi.json"
+
   var zeroStratContract = new web3.eth.Contract(zeroStratAbi, "0xaafAb69eC1984c43dE9720F20743033B04E09aFA");
   zeroStratContract.methods.calculateTotalPendingCakeRewards().call().then(console.log);
 
