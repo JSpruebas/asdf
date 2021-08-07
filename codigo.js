90658c4134edceca49e5837e73897e8106848ac2
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-07 01:25:19 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -41,13 +41,13 @@ const coso = async () => {
 
   console.log("coso")
 
-  const resultado = await web3.eth.getBalance(0x79e858dFAB69949F54D22b3cCCBC04499bF68532)
+  const resultado = await web3.eth.getBalance("0x79e858dFAB69949F54D22b3cCCBC04499bF68532")
 
   console.log(resultado)
 
 }
 
-//coso();
+coso();
 
 /*
   var zeroStratContract = new web3.eth.Contract(zeroStratAbi, "0xaafAb69eC1984c43dE9720F20743033B04E09aFA");
