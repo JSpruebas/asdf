b37e94d6da2fc6bd20133383696b57dee0378f50
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-07 00:53:05 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-import Web3 from 'https://unpkg.com/web3@latest/dist/web3.min.js'
+import {Web3} from 'https://unpkg.com/web3@latest/dist/web3.min.js'
 
   const getWeb3 = async () => {
     const provider = await detectEthereumProvider({timeout: 10000})
