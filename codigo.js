d016ec737e42893f3954c0b6e033c712f1d7e8eb
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-07 01:19:44 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,49 +1,49 @@
 web3 = new Web3
 
-  const getWeb3 = async () => {
-    const provider = await detectEthereumProvider({timeout: 10000})
-    if (provider) {
-      provider.on('chainChanged', () => location.reload())
-      provider.on('networkChanged', () => location.reload())
-      provider.on('accountsChanged', () => location.reload())
-      provider.on('disconnect', () => location.reload())
-  
-      await provider.request({ method: 'eth_requestAccounts' })
+const getWeb3 = async () => {
+  const provider = await detectEthereumProvider({ timeout: 10000 })
+  if (provider) {
+    provider.on('chainChanged', () => location.reload())
+    provider.on('networkChanged', () => location.reload())
+    provider.on('accountsChanged', () => location.reload())
+    provider.on('disconnect', () => location.reload())
 
-      web3.setProvider(provider)
+    await provider.request({ method: 'eth_requestAccounts' })
 
-      //web3 = new Web3(provider)     
-  
-      const result = {
-        injectedWeb3: true,
-        provider,
-        web3 () {
-          return web3
-        }
+    web3.setProvider(provider)
+
+    //web3 = new Web3(provider)     
+
+    const result = {
+      injectedWeb3: true,
+      provider,
+      web3() {
+        return web3
       }
-      result.networkId = await web3.eth.net.getId()
-      result.coinbase = await web3.eth.getCoinbase()
-      result.balance = await web3.eth.getBalance(result.coinbase)
-      LocalStorage.set('account-connected', 'true')
-      return result
-    } 
-    console.error('Web3 provider not detected')
-    return {injectedWeb3: false}
+    }
+    result.networkId = await web3.eth.net.getId()
+    result.coinbase = await web3.eth.getCoinbase()
+    result.balance = await web3.eth.getBalance(result.coinbase)
+    //LocalStorage.set('account-connected', 'true')
+    return result
   }
+  console.error('Web3 provider not detected')
+  return { injectedWeb3: false }
+}
 
-  getWeb3();
+getWeb3();
 
-  const coso = async () => {
+const coso = async () => {
 
-    console.log("coso")
+  console.log("coso")
 
-    const resultado = await web3.eth.getBalance(0x79e858dFAB69949F54D22b3cCCBC04499bF68532)
+  const resultado = await web3.eth.getBalance(0x79e858dFAB69949F54D22b3cCCBC04499bF68532)
 
-    console.log(resultado)
+  console.log(resultado)
 
 }
 
-  coso();
+coso();
 
 /*
   var zeroStratContract = new web3.eth.Contract(zeroStratAbi, "0xaafAb69eC1984c43dE9720F20743033B04E09aFA");
