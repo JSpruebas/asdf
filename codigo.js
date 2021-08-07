d085e85c948be1b5fb4603e811fba41cc5c1da37
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-07 01:13:42 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -10,7 +10,7 @@ const web3 = new Web3
   
       await provider.request({ method: 'eth_requestAccounts' })
 
-      const web3 = new Web3(provider)     
+      web3 = new Web3(provider)     
   
       const result = {
         injectedWeb3: true,
