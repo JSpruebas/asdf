a77a4072454f445a0f86715adbb224e80c232f2c
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-08 20:00:20 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -2,7 +2,7 @@
 const web3 = new Web3;
 
 window.onload = async () => {
-  const provider = await detectEthereumProvider({ timeout: 10000 })
+  const provider = await detectEthereumProvider({ timeout: 2000 })
   if (provider) {
     provider.on('chainChanged', () => location.reload())
     provider.on('accountsChanged', () => location.reload())
@@ -11,15 +11,13 @@ window.onload = async () => {
     await provider.request({ method: 'eth_requestAccounts' })
 
     web3.setProvider(provider)
-    // web3.eth.setProvider(provider)
-
-    //web3 = new Web3(provider)     
 
     coso();
-    
+
+  } else {
+    console.error('Web3 provider not detected')
+    alert("No se detecta Meta")
   }
-  console.error('Web3 provider not detected')
-  alert("No se detecta Meta")
 }
 
 const coso = async () => {
@@ -30,7 +28,7 @@ const coso = async () => {
     let tuBalance = await web3.eth.getBalance(tuCuenta[0]);
     tuBalance = web3.utils.fromWei(tuBalance);
     document.getElementById("bal").innerText = tuBalance;
-  } catch (err) { console.error(err); }
+  } catch (err) { console.error(err) }
 
   const zeroStratContract = await new web3.eth.Contract(window.abi1, "0xaafAb69eC1984c43dE9720F20743033B04E09aFA");
   let pendingReward = await zeroStratContract.methods.calculateTotalPendingCakeRewards().call();
