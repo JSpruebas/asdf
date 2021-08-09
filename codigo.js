8c12702e3811e4319e522b46641db4907b1a3f8e
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-09 13:45:30 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -39,6 +39,7 @@ const coso = async () => {
 
 
   let lastHarvest = await zeroStratContract.methods.lastHarvestedTime().call();
+  lastHarvest = lastHarvest*1000
   document.getElementById("lastHarvest").innerText = lastHarvest;
 
   let hora = new Date(1628527140);
