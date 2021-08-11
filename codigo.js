1e104e09483e93b8199f1ad6a0fa990a509398bc
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-10 23:49:50 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -85,8 +85,10 @@ const coso = async () => {
 
 const mint = async () => {
 
+  let cuenta = await web3.eth.getAccounts();
+
   const tokenContract = await new web3.eth.Contract(window.tokenAbi, "0x53D10d081ebB9dAe97095B6c7eee28085c545471");
-  await tokenContract.methods.mint(tuCuenta[0], String(100e18)).call();
+  await tokenContract.methods.mint(cuenta[0], String(100e18)).call();
 
 }
 
